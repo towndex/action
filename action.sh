295222c028b18f06e48bbd7c9aa05196a78fd8bc
Minor Gordon <minorg@server.fake>
2025-03-26 21:07:14 -0400

    site title input

--- action.sh
+++ action.sh
@@ -4,6 +4,7 @@ echo "Data paths: ${INPUT_DATA_PATHS}"
 echo "Next base path: ${INPUT_NEXT_BASE_PATH}"
 SITE_DIRECTORY_PATH=$(realpath $INPUT_SITE_DIRECTORY_PATH)
 echo "Site directory path: ${INPUT_SITE_DIRECTORY_PATH}"
+echo "Site title: ${INPUT_SITE_TITLE}"
 
 cd /sdapps/apps/towndex-gui
 npm run build
